9328d8483ebf4217100e6de1152354ae911247df
musitive <dallinfrank@gmail.com>
2017-10-05 09:08:28 -0600

    changes

--- searchUtility.js
+++ searchUtility.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
         var value = $("#searchField").val();
         console.log(value);
 
-        var myurl = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1&tags=taco";
-        
+        var myurl = "http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet";
 
         $.ajax({
-            url: myurl, // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-            type: 'POST', // The HTTP Method
-            data: {}, // Additional parameters here
-            datatype: 'json',
-            success: function(data) { alert("You are a bold one."); },
-            error: function(err) { alert(err); },
-            beforeSend: function(xhr) {
-            xhr.setRequestHeader("X-Mashape-Authorization", "7Rvrgzc0WNmshVRiCKUvl0pRLwIHp1j8HF5jsntFPf4R47ij8Y"); // Enter here your Mashape key
+            url : myurl,
+            dataType : "json",
+            success : function(parsed_json) {
+                alert("You are a bold one.");
+            },
+            error: function(err,data) {
+                console.log(err);
+                console.log(data);
             }
         });
+
 	});
 });
